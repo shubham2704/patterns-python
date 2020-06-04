556a892f98da412bb6f15e5dc5f4151247dde6dc
shubham goswami <cool.shhubham2704@gmail.com>
2020-06-04 22:03:57 +0530

    added 13-24

---
+++ pattern-13.py
@@ -0,0 +1,15 @@
+for x in range(65,70):
+    for y in range(65,x+1):
+        print(chr(x),end='')
+    print()
+
+"""
+# p[attern 
+
+A
+BB
+CCC
+DDDD
+EEEEE
+
+"""
---
+++ pattern-18.py
@@ -0,0 +1,4 @@
+for x in range(5):
+    for y in range(4,x-1,-1):
+        print(chr(x+65),end='')
+    print()
