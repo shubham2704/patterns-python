9b6fa09b3dccbd60d1ee4a4c28623c40f496b658
shubham goswami <cool.shhubham2704@gmail.com>
2020-06-07 00:23:27 +0530

    added new pattern

---
+++ pattern-26.py
@@ -0,0 +1,19 @@
+wh =  4
+wl = 4
+
+for x in range(1,wh+1):
+    for y in range(1,wl+1):
+        if x!=wh:
+            print(" ",end='')
+        else:
+            print('*',end='')
+        for z in range(1,5):
+            if x==1 or z==1 or z==4:
+                print('*',end='')
+            else:
+                print(" ",end='')
+        if x!=wh:
+            print("  ",end='')
+        else:
+            print('*',end='')
+    print()
